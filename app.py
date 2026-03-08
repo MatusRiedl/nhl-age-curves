140465d91a7841afa466feedd363d46e3aac8146
Matus Riedl <iksperial@gmail.com>
2026-03-08 16:40:53 +0100

    Update

--- app.py
+++ app.py
@@ -412,7 +412,7 @@ st.markdown("---")
 # Keep this visible version synced with the newest changelog entry
 st.markdown(
     "<p style='text-align:center;color:gray;font-size:14px;'>"
-    "Created by Iksperial. v0.87.0 -- 7095 lines of Python<br>"
+    "Created by Iksperial. v0.90.0 -- 7370 lines of Python<br>"
     "<em>Data is the only religion that strictly punishes you for ignoring it.</em>"
     "</p>",
     unsafe_allow_html=True,
